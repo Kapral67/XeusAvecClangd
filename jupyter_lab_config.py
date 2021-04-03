27bfbfe0d4b8836ecc680ca23ecaece8bdc6764f
Kapral67 <kapralmaxwell@gmail.com>
2021-04-03 04:46:35 -0700

    Update jupyter_lab_config.py

--- jupyter_lab_config.py
+++ jupyter_lab_config.py
@@ -848,7 +848,7 @@ import shutil
 c.LanguageServerManager.language_servers = {
     "clangd": {
         # if installed as a binary
-        "argv": [shutil.which("clangd", path="/home/jovyan/clangd-linux-11.0.0/bin/"), "-pretty"],
+        "argv": [shutil.which("clangd", path="/home/jovyan/clangd_11.0.0/bin/"), "-pretty"],
         "languages": ["c++src", "C++src()", "c++src()", "C++src"],
         "version": 2
     }
