18cb8831343915e3e30a7a69c61ba0ec17467179
Kapral67 <kapralmaxwell@gmail.com>
2021-08-09 03:19:35 -0700

    Preminlinary Update to jupyter config py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -14,7 +14,7 @@ c.ContentsManager.allow_hidden = True
 c.LanguageServerManager.language_servers = {
     "clangd": {
         # if installed as a binary
-        "argv": [shutil.which("clangd", path="/home/jovyan/clangd_11.0.0/bin/"), "-pretty"],
+        "argv": [shutil.which("clangd", path="/home/jovyan/**CHANGE_THIS**/bin/"), "-pretty", "--clang-tidy", "--clang-tidy-checks='modernize*, readability*, performance*, bugprone*, cppcoreguidelines*'", "--completion-style=detailed", "--suggest-missing-includes", "--pch-storage=memory", "--log=verbose"],
         "languages": ["c++src", "csrc"],
         "version": 2
     }
